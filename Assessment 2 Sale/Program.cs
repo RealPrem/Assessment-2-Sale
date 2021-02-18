694564cdda0e9572f3f8a360b9b732bda26a2d0f
prem@ascot.ac.th <prem@ascot.ac.th>
2021-02-18 15:02:17 +0700

    Extra Question

--- Assessment 2 Sale/Program.cs
+++ Assessment 2 Sale/Program.cs
@@ -17,6 +17,32 @@ namespace Assessment_2_Sale
             Console.WriteLine(salesPeople[0].GetCount());
             Console.WriteLine(salesPeople[1].GetSalesHistory(0).GetValue());
             Console.WriteLine(salesPeople[0].calcTotalSales());
+
+            Console.WriteLine("HIGHEST");
+            Highest(salesPeople);
+
+        }
+        public static string Highest(SalesPerson[] salesPeople)
+        {
+            double Highest;
+            string Person = "";
+            Highest = salesPeople[0].calcTotalSales();
+            for (int i = 0; i < salesPeople.Length; i += 1)
+            {
+                if (salesPeople[i] == null)
+                {
+                    break;
+                }
+                if (salesPeople[i].calcTotalSales() > Highest)
+                {
+                    Console.WriteLine(salesPeople[i].GetID());
+                    Console.WriteLine(salesPeople[i].calcTotalSales());
+
+                    Highest = salesPeople[i].calcTotalSales();
+                    Person = salesPeople[i].GetID();
+                }
+            }
+            return Person;
         }
     }
 }
